201751be39abe19f9544cdd7a32defadcfdaf6b2
havgirl <114887886+havgirl@users.noreply.github.com>
2023-01-12 16:07:13 -0500

    Module14.6.1 Earthquake mapping

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -35,20 +35,17 @@ L.control.layers(baseMaps).addTo(map);
 let torontoData = "https://raw.githubusercontent.com/havgirl/Mapping_Earthquakes/main/torontoRoutes.json";
 
 // Create a style for the lines.
-let myStyle = {
-  color: "#ffffa1",
-  weight: 2
-}
 
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJSON(data {
-  style: myStyle,
+  color: "#ffffa1",
+  weight: 2,
   onEachFeature: function(feature, layer){
-    layer.bindPopu("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
-    + feature.properties.dst + "</h3>")
+    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
+    + feature.properties.dst + "</h3>");
   }
 }).addTo(map);
 });
